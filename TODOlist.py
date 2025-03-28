8c4eea6d0a1859f00ba0e9ed5f3802ca09413e86
sKarab372 <kpbcosmic@gmail.com>
2025-03-28 19:32:33 +0530

    Add files via upload

---
+++ TODOlist.py
@@ -0,0 +1,35 @@
+todo = []
+flag = True
+while flag:
+    print("1.Enter a new task")
+    print("2.View tasks")
+    print("3.Delete a task")
+    print("4.Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        task = input("Enter the task: ")
+        todo.append(task)
+        print("Task added successfully.")
+    elif choice == 2:
+        if len(todo) == 0:
+            print("No tasks available.")
+        else:
+            print("Tasks:")
+            for i, task in enumerate(todo):
+                print(f"{i + 1}. {task}")
+    elif choice == 3:
+        if len(todo) == 0:
+            print("No tasks available.")
+        else:
+            print("Tasks:")
+            for i, task in enumerate(todo):
+                print(f"{i + 1}. {task}")
+            task_num = int(input("Enter the task number to delete: "))
+            if 1 <= task_num <= len(todo):
+                deleted_task = todo.pop(task_num - 1)
+                print(f"Task '{deleted_task}' deleted successfully.")
+            else:
+                print("Invalid task number.")
+    elif choice == 4:
+        print("Bye, bye!!!")
+        flag = False
